e85084eddeb153167b9cad26878001d51f701e04
rferguson <rferguson@industrialagency.ca>
2019-05-30 14:55:12 -0400

    added init script

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum -y install unzip && wget https://github.com/industrialdev/setup-centos-lemp/archive/master.zip && unzip master.zip && source setup-centos-lemp-master/setup_centos_server.sh
+
+
+
+
+
+
