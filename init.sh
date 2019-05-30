ef0a6a3fe34df656e03bc303154456109334e119
rferguson <rferguson@industrialagency.ca>
2019-05-30 15:08:58 -0400

    added wget to list of needed software

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum -y install unzip && wget https://github.com/industrialdev/setup-centos-lemp/archive/master.zip && unzip master.zip && source setup-centos-lemp-master/setup_centos_server.sh
+sudo yum -y install unzip wget && wget https://github.com/industrialdev/setup-centos-lemp/archive/master.zip && unzip master.zip && source setup-centos-lemp-master/setup_centos_server.sh
 
 
 
