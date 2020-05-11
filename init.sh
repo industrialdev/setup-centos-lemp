f434578b01c67bc1becb265b3f7b943c29896e2c
rferguson10 <rferguson@industrialagency.ca>
2020-05-11 14:55:02 -0400

    Update init.sh
    Updated filepaths

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum -y install unzip wget && wget https://github.com/industrialdev/setup-centos-lemp/archive/master.zip && unzip master.zip && cd setup-centos-lemp-master && source setup_centos_server.sh
+sudo yum -y install unzip wget && wget https://github.com/industrialdev/setup-centos7-lemp/archive/master.zip && unzip master.zip && cd setup-centos7-lemp-master && source setup_centos_server.sh
 
 
 
